512fbb9db98fc204ff672adc2d0bfb037948b584
Gingy_III <70298285+MujtabaaAdam@users.noreply.github.com>
2023-09-14 20:46:44 -0500

    Added a basic guessing game (have to optimize it).

--- bot.py
+++ bot.py
@@ -20,6 +20,29 @@ async def ping(ctx):
     await ctx.send('Pong!')
 
 
+
+@client.command()
+async def guess(ctx, max:int=2):
+    num = random.randint(1,max)
+    attempts = 5
+    await ctx.send(f'Guessing game started!\nPlease guess a number between **1-{max}**')
+
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.message.channel
+    while attempts >= 0:
+        guess = await client.wait_for('message', check=check)
+        if guess.content == str(num):
+            await ctx.send('You guessed correctly!')
+            attempts = 0
+        else:
+            await ctx.send(f'Whoops... try again. You have **{attempts}** more attempts')
+            attempts -= 1
+
+        
+
+
+
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 client.run(TOKEN)
 
